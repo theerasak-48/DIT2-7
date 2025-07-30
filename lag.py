dcb9eac6665e441a403baa62c25bfd865d81aa33
theerasak-48 <gubetama48@gmail.com>
2025-07-30 12:48:32 +0700

    Add files via upload

---
+++ lag.py
@@ -0,0 +1,8 @@
+a = int(input("ใส่เลข: "))
+b = int(input("ใส่เลข: "))
+c = int(input("ใส่เลข: "))
+d = int(input("ใส่เลข: "))
+z = int(input("ใส่เลข: "))
+number_list = [a,b,c,d,z]
+number_list.sort()
+print(number_list)
